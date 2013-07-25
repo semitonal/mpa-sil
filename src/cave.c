60a7ddf6fe668b4d0b459a53bc267bce20967fca
MarvinPA <chriscampbell89@gmail.com>
2013-07-25 06:28:41 +0100

    Recolour walls and floors instead of monsters while raging (cherry picked from commit de07c45a31d954e90338225cbcf7cffa8c450cb8)
    Conflicts:
    	src/cave.c
    	src/xtra2.c

--- src/cave.c
+++ src/cave.c
@@ -949,13 +949,6 @@ void map_info(int y, int x, byte *ap, char *cp, byte *tap, char *tcp)
 	/* Cave flags */
 	info = cave_info[y][x];
 
-	bool hide_square = FALSE;
-	bool blood_red = FALSE;
-	
-	// 'rage' effects...
-	if ((!p_ptr->is_dead) && p_ptr->rage && !(info & (CAVE_SEEN))) hide_square = TRUE;
-	if ((!p_ptr->is_dead) && p_ptr->rage)                          blood_red = TRUE;
-
 	/* make sure not to display things off screen */
 	if ((y < 0) || (x < 0) || (y >= p_ptr->cur_map_hgt) || (x >= p_ptr->cur_map_wid))
 	{
@@ -985,20 +978,7 @@ void map_info(int y, int x, byte *ap, char *cp, byte *tap, char *tcp)
 			c = PICT_C(i);
 		}
 	}
-    
-    // hiding squares out of line of sight during rage
-	else if (hide_square)
-	{
-		/* Get the darkness feature */
-		f_ptr = &f_info[FEAT_NONE];
-		
-		/* Normal attr */
-		a = f_ptr->x_attr;
-		
-		/* Normal char */
-		c = f_ptr->x_char;
-	}
-    
+
 	/* Boring grids (floors, etc) */
 	else if (cave_floorlike_bold(y,x))
 	{
@@ -1017,6 +997,11 @@ void map_info(int y, int x, byte *ap, char *cp, byte *tap, char *tcp)
 
 			/* Special lighting effects */
 			special_lighting_floor(&a, &c, info, cave_light[y][x]);
+
+			if (!p_ptr->is_dead && p_ptr->rage && (info & (CAVE_SEEN)))
+			{
+				a = TERM_RED;
+			}
 		}
 
 		/* Unknown */
@@ -1053,6 +1038,11 @@ void map_info(int y, int x, byte *ap, char *cp, byte *tap, char *tcp)
 
 			/* Special lighting effects (walls only) */
 			special_lighting_wall(&a, &c, feat, info);
+
+			if (!p_ptr->is_dead && p_ptr->rage && (info & (CAVE_SEEN)))
+			{
+				a = TERM_RED;
+			}
 		}
 
 		/* Unknown */
@@ -1080,7 +1070,7 @@ void map_info(int y, int x, byte *ap, char *cp, byte *tap, char *tcp)
 		{
 			
 			/* Memorized objects */
-			if (o_ptr->marked && !hide_square)
+			if (o_ptr->marked)
 			{
 				
 				/* Normal attr */
@@ -1118,7 +1108,7 @@ void map_info(int y, int x, byte *ap, char *cp, byte *tap, char *tcp)
 	}
 
 	/* Monsters */
-	if ((m_idx > 0) && !hide_square)
+	if (m_idx > 0)
 	{
 		monster_type *m_ptr = &mon_list[m_idx];
         monster_race *r_ptr = &r_info[m_ptr->r_idx];
@@ -1194,11 +1184,6 @@ void map_info(int y, int x, byte *ap, char *cp, byte *tap, char *tcp)
 			{
 				/* Normal attr */
 				a = da;
-			}
-			
-			if (blood_red)
-			{
-				a = TERM_RED;
 			}
 			
 			if (use_background_colors)
